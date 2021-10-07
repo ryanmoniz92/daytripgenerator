e4180ee2fc9374437d95617b0254df157bd2eb26
Ryan Moniz <ryanmoniz@Ryans-MacBoook-Air.local>
2021-10-07 17:50:59 -0400

    Added loop to account for wrong response.

--- daytripgenerator.py
+++ daytripgenerator.py
@@ -11,6 +11,7 @@ def random_generator(some_list):
     some_value = random.choice(some_list)
     return some_value
 
+
 def random_trip():
     email = input('Please enter your email: ')
     dest = random_generator(destinations)
@@ -18,6 +19,13 @@ def random_trip():
     trans = random_generator(transportations)
     ent = random_generator(entertainments)
     prompt_of_list = input(f'Destination: {dest} \nRestaurant: {rest} \nTransport: {trans} \nEntertainment: {ent} \nTo confirm trip, please type "Confirm". If you would like to generate a new trip, please type "New". ')
+    while prompt_of_list != 'Confirm' or prompt_of_list != 'New':
+        if prompt_of_list == 'Confirm':
+            break
+        if prompt_of_list == 'New':
+            break
+        else:
+            prompt_of_list = input('Invalid response, please try again: ')   
     while prompt_of_list == 'Confirm' or prompt_of_list == 'New':
         if prompt_of_list == 'Confirm':
             print(f'Trip confirmed! An email with all details was sent to {email}!')
@@ -28,7 +36,7 @@ def random_trip():
             trans = random_generator(transportations)
             ent = random_generator(entertainments)
             prompt_of_list = input(f'Destination: {dest} \nRestaurant: {rest} \nTransport: {trans} \nEntertainment: {ent} \nTo confirm trip, please type "Confirm". If you would like to generate a new trip, please type "New". ')
-            return
+            
 
 random_trip()
 
